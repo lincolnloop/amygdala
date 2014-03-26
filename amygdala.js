deecea2d7723de1b94d84354b16e9bfe7bd75654
bkonkle <brandon@konkle.us>
2014-03-26 10:58:48 -0500

    Split ajax and serialize into their own lib modules

--- amygdala.js
+++ amygdala.js
@@ -1,69 +1,8 @@
 'use strict';
 
 var _ = require('underscore');
+var ajax = require('./lib/ajax');
 var log = require('loglevel');
-var Q = require('q');
-
-
-// ------------------------------
-// Utility functions
-// ------------------------------
-
-function serialize(obj) {
-  log.debug('serialize', obj);
-  // Translates an object to a querystring
-  if (!_.isObject(obj)) {
-    return obj;
-  }
-  var pairs = [];
-  _.each(obj, function(value, key) {
-    pairs.push(encodeURIComponent(key) + '=' + encodeURIComponent(value));
-  });
-  return pairs.join('&');
-}
-
-function ajax(method, url, options) {
-  // Sends a GET request, converting the data into a querystring
-  //
-  // method: GET, POST, PUT, DELETE
-  // url: the url to send the request to
-  // options: extra options
-  // - data: converted to a querystring for GET requests
-  // - contentType: a value for the Content-Type request header
-  var query;
-  options = options || {};
-
-  if (!_.isEmpty(options.data) && method === 'GET') {
-    query = serialize(options.data);
-    url = url + '?' + query;
-  }
-
-  var request = new XMLHttpRequest();
-  var deferred = Q.defer();
-
-  request.open(method, url, true);
-
-  request.onload = function() {
-    if (request.status === 200) {
-      deferred.resolve(request.response);
-    } else {
-      deferred.reject(new Error('Request failed with status code ' + request.status));
-    }
-  };
-
-  request.onerror = function() {
-    deferred.reject(new Error('Unabe to send request to ' + JSON.stringify(url)));
-  };
-
-  if (!_.isEmpty(options.contentType)) {
-    request.setRequestHeader('Content-Type', options.contentType);
-  }
-
-  request.send(method === 'GET' ? null : options.data);
-
-  return deferred.promise;
-}
-
 
 var Amygdala = function(schema) {
   // Session/browser key/value store with remote sync capabilities.
